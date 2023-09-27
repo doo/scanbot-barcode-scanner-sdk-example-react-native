a0ff277fb14e66a6f18ba93bba59dcb96e15716e
Marco <marco@scanbot.io>
2023-09-27 17:55:50 +0200

    Disabled flipper for Android < 6
    * https://github.com/facebook/flipper/issues/3572
    * Issue EPIC-3803

--- latest/android/app/src/main/java/io/scanbot/example/sdk/barcode/reactnative/MainApplication.java
+++ latest/android/app/src/main/java/io/scanbot/example/sdk/barcode/reactnative/MainApplication.java
@@ -1,6 +1,7 @@
 package io.scanbot.example.sdk.barcode.reactnative;
 
 import android.app.Application;
+import android.os.Build;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -60,6 +61,12 @@ public class MainApplication extends Application implements ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+
+    // WORKAROUND FOR ISSUE https://github.com/facebook/flipper/issues/3572
+    // The latest version of flipper does not work on Android < 6.
+    // Until the issue is officially addressed we have to disable flipper.
+    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
   }
 }
