0952c54c489260d6a045e41e33ba213b2b37a859
Marko <marko@scanbot.io>
2025-01-15 11:36:51 +0100

    Add code snippet for BarcodeCameraView with overlay view binder

---
+++ src/codeSnippets/scanbotBarcodeCameraViewWithOverlayViewBinder.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  BarcodeItemOverlayViewConfig,
+  BarcodeResultField,
+  BarcodeScannerResult,
+  ScanbotBarcodeCameraView,
+} from 'react-native-scanbot-barcode-scanner-sdk';
+
+export function BarcodeCameraViewWithOverlayViewBinder() {
+  return (
+    <ScanbotBarcodeCameraView
+      finderConfig={{
+        viewFinderEnabled: true,
+        overlayColor: '#000000A9',
+      }}
+      selectionOverlayConfig={{
+        overlayEnabled: true,
+        loadingTextValue: 'Please wait...', // Optional property, useful if you query your server to check the barcode.
+        barcodeItemOverlayViewBinder: (
+          barcodeItem: BarcodeResultField,
+        ): Promise<BarcodeItemOverlayViewConfig> | BarcodeItemOverlayViewConfig => {
+          /** TODO: process scan result as needed,
+           * e.g. query your server to check the barcode.
+           *
+           * See example below.
+           */
+          const text = barcodeItem.type === 'DATA_MATRIX' ? 'Valid barcode' : 'Invalid barcode';
+          const color = barcodeItem.type === 'DATA_MATRIX' ? '#00FF00' : '#FF0000';
+
+          return {
+            text: text,
+            textColor: '#FFFFFF',
+            textContainerColor: color,
+            strokeColor: color,
+            // Configure other parameters as needed.
+          };
+        },
+      }}
+      onBarcodeScannerResult={(result: BarcodeScannerResult) => { }}
+    />
+  );
+}
